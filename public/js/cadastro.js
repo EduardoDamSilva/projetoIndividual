1cfff2c4731a884d68db87593ff2bcb01d576818
EduardoDamSilva <eduardo.silva@sptech.school>
2022-10-09 15:48:30 -0300

    script de cadastro

--- public/js/cadastro.js
+++ public/js/cadastro.js
@@ -19,10 +19,31 @@ function validarCadastro(){
   var confirmarSenha = inputConfirmarSenha.value
 
   if(nomeUsuario.length < 6){
-    pAvisoNomeUsuario.style.display = "block"
-    inputNomeUsuario.style.backgroundColor = "#F3E3F4"
-    inputNomeUsuario.style.border = "2px solid #EEBBF1"
-  } else if (email == ''){
+    exibirAviso(pAvisoNomeUsuario, inputNomeUsuario, "Seu nome de usuário deve conter mais de 6 dígitos")
+  }
 
+  if (email == ""){
+    exibirAviso(pAvisoEmail, inputEmail, "Insira um email")
+  }
+
+  if(email.indexOf("@") < 3 || email.indexOf(" ") >= 0 || !email.indexOf(".")){
+    exibirAviso(pAvisoEmail, inputEmail, "Email inválido")
+  }
+
+  if(senha.length < 8){
+    exibirAviso(pAvisoSenha, inputSenha, "Senha muito curta")
+  }
+  
+  if(senha != confirmarSenha){
+    exibirAviso(pAvisoConfirmarSenha, inputConfirmarSenha, "Senhas incompátiveis")
   }
 }
+
+function exibirAviso(paragrafoTag, input, mensagem){
+  paragrafoTag.innerHTML = mensagem
+  paragrafoTag.style.display = "block"
+  input.style.backgroundColor = "#F3E3F4"
+  input.style.border = "2px solid #EEBBF1"
+}
+
+
